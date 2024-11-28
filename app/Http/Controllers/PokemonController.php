990ac797b5f8afd171864559e6ca499fcaf11e18
ViniciusMat0s <vinibmatos@rede.ulbra.br>
2024-11-28 10:33:25 -0300

    edit-pokemoncontroller

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -3,8 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use App\Models\Coach;
+use App\Models\Pokemon;
 
 class PokemonController extends Controller
 {
-    //
+    public function index()
+    {
+        $pokemon = Pokemon::all();
+        return view('pokemon.index', compact('pokemon'));
+    }
+
+    public function create()
+    {
+        return view('pokemon.create');
+    }
+
+    public function store(Request $request)
+    {
+        Pokemon::create($request->all());
+        return redirect('pokemon')->with('success', 'Pokemon created successfully.');
+    }
+
+    public function edit($id)
+    {
+        $pokemon = Pokemon::findOrFail($id);
+        return view('pokemon.edit', compact('pokemon'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $pokemon = Pokemon::findOrFail($id);
+        $pokemon->update($request->all());
+        return redirect('pokemon')->with('success', 'Pokemon updated successfully.');
+    }
+
+    public function destroy($id)
+    {
+        $pokemon = Pokemon::findOrFail($id);
+        $pokemon->delete();
+        return redirect('pokemon')->with('success', 'Pokemon deleted successfully.');
+    }
 }
