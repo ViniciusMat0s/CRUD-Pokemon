6f96d293dff425f72b20c56a1348349c4aa4c2da
ViniciusMat0s <vinibmatos@rede.ulbra.br>
2024-11-28 10:36:51 -0300

    edit-migrations-pokemon

--- database/migrations/2024_11_28_133048_create_pokemon_table.php
+++ database/migrations/2024_11_28_133048_create_pokemon_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
     {
         Schema::create('pokemon', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 200);
+            $table->string('name', 100);
             $table->string('type', 100);
             $table->integer('power');
+            $table->text('image');
+            $table->foreignId('coach_id')->references('id')->on('coaches');
             $table->timestamps();
-            $table->text('image'); 
         });
     }
 
