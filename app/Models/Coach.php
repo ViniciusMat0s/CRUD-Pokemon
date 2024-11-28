65940af1a97f4809d5dd3277a965a84b842a2b3b
ViniciusMat0s <vinibmatos@rede.ulbra.br>
2024-11-28 10:37:36 -0300

    add-function-model-coach

--- app/Models/Coach.php
+++ app/Models/Coach.php
@@ -7,7 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Coach extends Model
 {
     protected $fillable = [
-       'image',
-       'name' 
+        'image',
+        'name',
     ];
+
+    public function pokemon()
+    {
+        return $this->hasMany(Pokemon::class);
+    }
 }
